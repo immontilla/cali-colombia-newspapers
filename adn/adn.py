1b68f21f614be4726a311f79253f2459008f7cb8
Iván <mmontilla@serbatic.es>
2022-03-01 11:54:08 +0100

    Adding alternative urls to look for the Cali edition of the ADN newspaper

--- adn/adn.py
+++ adn/adn.py
@@ -1,5 +1,4 @@
 import socket
-import uuid
 import requests
 import urllib.request
 import os
@@ -9,18 +8,31 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 from PIL import Image
 
 
-def day_month_year():
+def day_month_year(pattern):
     now = datetime.now()
-    return now.strftime("%d%m%Y")
+    return now.strftime(pattern)
 
 
-def get_issuu_url(issuu_url):
-    return issuu_url + day_month_year()
+def magazine_urls():
+    names = ['adn_cali_-_', 'adn_cali_']
+    patterns = ['%d%m%Y','%Y%m%d']
+    urls = []
+    for name in names:
+        for pattern in patterns:
+            urls.append(MAGAZINE_URL + name + day_month_year(pattern))
+    return urls
 
 
-def get_uuid(issuu_url):
-    request = requests.get(issuu_url)
-    if request.status_code != 200:
+def images_magazine_url():
+    magazine_url = None
+    urls = magazine_urls()
+    for url in urls:
+        request = requests.get(url)
+        print('Trying to get', url,'...')
+        if request.status_code == 200:
+            magazine_url = url
+            break
+    if magazine_url is None:
         return None
     source = request.text
     soup = BeautifulSoup(source, features="html.parser")
@@ -29,17 +41,17 @@ def get_uuid(issuu_url):
     start = link.find(IMAGE_URL_DOMAIN) + len(IMAGE_URL_DOMAIN)
     end = link.rfind('/jpg/page_1.jpg')
     uuid = link[start:end]
-    return uuid
+    return IMAGE_URL_DOMAIN + str(uuid) + '/jpg/'
 
 
-def magazine_as_pdf(uuid):
+def magazine_as_pdf(url):
     magazine = PdfFileMerger()
     index = 1
     last_page = False
     while not last_page:
         filename = 'page_' + str(index)
         img_file = filename + '.jpg'
-        img_file_url = IMAGE_URL_DOMAIN + str(uuid) + '/jpg/' + img_file
+        img_file_url = url + img_file
         try:
             urllib.request.urlretrieve(img_file_url, img_file)
             image = Image.open(img_file)
@@ -52,19 +64,17 @@ def magazine_as_pdf(uuid):
             index += 1
         except Exception:
             last_page = True
-    magazine_pdf = 'adn_' + day_month_year() + '.pdf'
+    magazine_pdf = 'adn_' + day_month_year("%Y_%m_%d") + '.pdf'
     magazine.write(magazine_pdf)
     return magazine_pdf
 
 
-MAGAZINE_URL = 'https://issuu.com/diarioadn.co/docs/adn_cali_-_'
+MAGAZINE_URL = 'https://issuu.com/diarioadn.co/docs/'
 IMAGE_URL_DOMAIN = 'https://image.isu.pub/'
 socket.setdefaulttimeout(30)
-issuu_url = get_issuu_url(MAGAZINE_URL)
-uuid = get_uuid(issuu_url)
-if uuid is not None:
-    print('Analyzing', issuu_url)
-    pdf = magazine_as_pdf(uuid)
+images_magazine_url = images_magazine_url()
+if images_magazine_url is not None:
+    pdf = magazine_as_pdf(images_magazine_url)
     print(pdf, 'done!')
 else:
-    print(issuu_url, 'not found')
+    print('Today edition is not published online yet')
